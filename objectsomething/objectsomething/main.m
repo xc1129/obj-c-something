0b89795ae343fcc6032b72f9edc22767ed9bb6fd
Spirit_xc <xc1129@yeah.net>
2015-03-03 22:35:48 +0800

    create main.m

---
+++ objectsomething/objectsomething/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objectsomething
+//
+//  Created by Spirit_xc on 15/3/3.
+//  Copyright (c) 2015年 Spirit_xc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
